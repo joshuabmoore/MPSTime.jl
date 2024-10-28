af44f45f63a19b7c6cb831ae79e2066d12503a12
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-10-29 06:01:40 +1100

    ipd final benchmarks imputation

--- FinalBenchmarks/NASA_KeplerV2/train_kepler_interp.jl
+++ FinalBenchmarks/NASA_KeplerV2/train_kepler_interp.jl
@@ -1,6 +1,7 @@
 include("../../LogLoss/RealRealHighDimension.jl")
+include("../../Interpolation/imputation.jl");
 using JLD2
-dloc =  "Data/NASA_KeplerV2/datasets/contact_eb.jld2"
+dloc =  "/Users/joshua/Desktop/QuantumInspiredMLFinal/QuantumInspiredML/Data/NASA_kepler/c0/sample831v4.jld2"
 f = jldopen(dloc, "r")
     X_train = read(f, "X_train")
     y_train = read(f, "y_train")
@@ -18,14 +19,14 @@ encoding = :legendre_no_norm
 encode_classes_separately = false
 train_classes_separately = false
 
-d = 12
-chi_max=80
+d = 10
+chi_max=20
 
 opts=MPSOptions(; nsweeps=5, chi_max=chi_max,  update_iters=1, verbosity=verbosity, loss_grad=:KLD,
     bbopt=:TSGO, track_cost=track_cost, eta=0.1, rescale = (false, true), d=d, aux_basis_dim=2, encoding=encoding, 
     encode_classes_separately=encode_classes_separately, train_classes_separately=train_classes_separately, 
-    exit_early=false, sigmoid_transform=false, init_rng=4567, chi_init=4)
-
+    exit_early=false, sigmoid_transform=true, init_rng=4567, chi_init=4)
+opts_safe, _... = safe_options(opts, nothing, nothing)
 if test_run
     W, info, train_states, test_states, p = fitMPS(X_train, y_train,  X_test, y_test;  opts=opts, test_run=true)
     plot(p)
@@ -37,16 +38,30 @@ else
     #sweep_summary(info)
 end
 
-range = model_encoding(opts.encoding).range
+fc = load_forecasting_info_variables(W, X_train, y_train, X_test, y_test, opts_safe; verbosity=0)
+dx = 5e-3
+mode_range=(-1,1)
+xvals=collect(range(mode_range...; step=dx))
+mode_index=Index(opts_safe.d)
+xvals_enc= [get_state(x, opts_safe) for x in xvals]
+xvals_enc_it=[ITensor(s, mode_index) for s in xvals_enc];
+interp_sites = collect(61:92)
+stats, p1_ns = any_impute_single_timeseries(fc, 0, 6, interp_sites, :directMedian; 
+        invert_transform=true, 
+           NN_baseline=false, X_train=X_train, y_train=y_train, 
+            n_baselines=1, plot_fits=true, dx=dx, mode_range=mode_range, xvals=xvals, 
+            mode_index=mode_index, xvals_enc=xvals_enc, xvals_enc_it=xvals_enc_it)
+plot(p1_ns...)
+# range = model_encoding(opts.encoding).range
 
-X_train_scaled = transform_data(X_train; range=range, minmax_output=opts.minmax)
-X_test_scaled = transform_data(X_test; range=range, minmax_output=opts.minmax)
-svpath = "/Users/joshua/Desktop/QuantumInspiredML/Data/NASA_KeplerV2/mps_saves/eb_legendre_ns_d$(d)_chi$(chi_max).jld2"
-f = jldopen(svpath, "w")
-    write(f, "X_train_scaled", X_train_scaled)
-    write(f, "y_train", y_train)
-    write(f, "X_test_scaled", X_test_scaled)
-    write(f, "y_test", y_test);
-    write(f, "mps", W)
-    write(f, "opts", opts)
-close(f)
+# X_train_scaled = transform_data(X_train; range=range, minmax_output=opts.minmax)
+# X_test_scaled = transform_data(X_test; range=range, minmax_output=opts.minmax)
+# svpath = "/Users/joshua/Desktop/QuantumInspiredML/Data/NASA_KeplerV2/mps_saves/eb_legendre_ns_d$(d)_chi$(chi_max).jld2"
+# f = jldopen(svpath, "w")
+#     write(f, "X_train_scaled", X_train_scaled)
+#     write(f, "y_train", y_train)
+#     write(f, "X_test_scaled", X_test_scaled)
+#     write(f, "y_test", y_test);
+#     write(f, "mps", W)
+#     write(f, "opts", opts)
+# close(f)
