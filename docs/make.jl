99f24ad74ee0d549bfb7b1edc922f10a0ae22f0c
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-12-21 06:56:11 +1100

    update make.jl for deploymenton gh pages

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,10 @@ makedocs(
     ]
 )
 
+deploydocs(
+    repo = "github.com/jmoo2880/MPSTime.jl.git",
+)
+
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
