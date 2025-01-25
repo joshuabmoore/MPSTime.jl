8ca74d488306929655762240d02f21d38dd5be0a
jmoo2880 <joshua.moore@sydney.edu.au>
2025-01-25 16:13:27 +1100

    add subset selection info

--- TSAD/preprocessing/preproc.jl
+++ TSAD/preprocessing/preproc.jl
@@ -5,14 +5,19 @@ using ProgressMeter
 using StatsBase
 using PrettyTables
 using Plots.PlotMeasures
+using JLD2
 
-mutable struct dataset
-    name::String
-    X_train::Vector{Float64}
-    X_test::Vector{Float64}
-    X_anomaly::Vector{Float64}
-end
 
 function ucr_data_loader(fname::String)
-
+    data_f = readdlm(fname)
+    fname_split = split.(split(fname, "_"), ".")
+    dname = fname_split[4][1]
+    train_end = parse(Int, fname_split[5][1])
+    anomaly_start = parse(Int, fname_split[6][1])
+    anomaly_end = parse(Int, fname_split[7][1])
+    data = size(data_f, 1) > size(data_f, 2) ? data_f[:, 1] : data_f[1, :];
+    X_train = data[1:train_end] # inclusive of train end
+    X_test = data[train_end+1:end];
+    anom_range = (anomaly_start-length(X_train):anomaly_end-length(X_train))
+    return X_train, X_test, anom_range
 end
